5500a1336626acefc7ee8c6d684fb0105adf8a65
cegarciav <cegarcia2@uc.cl>
2019-11-10 20:05:50 +0100

    dtd actualizado para nuevo esquema

--- MOD 8.4 - BE 1 - XML (sujet)/create_xml_2.py
+++ MOD 8.4 - BE 1 - XML (sujet)/create_xml_2.py
@@ -9,14 +9,15 @@ with open("ponctualite-mensuelle-transilien.csv", encoding="ISO-8859-1") as file
 xml_style = '<?xml-stylesheet href="estilazo.css"  type="text/css"?>' \
             '<!DOCTYPE Services [' \
             '<!ELEMENT Services (Service+)>' \
-            '<!ELEMENT Service (ServiceNom, Ligne+)>' \
-            '<!ELEMENT Ligne (LigneID, LigneNom, Train)>' \
-            '<!ELEMENT Train (TrainID, Ponctualite+)>' \
+            '<!ELEMENT Service (Nom, Lignes)>' \
+            '<!ELEMENT Lignes (Ligne+)>' \
+            '<!ELEMENT Ligne (ID, Nom, Train)>' \
+            '<!ELEMENT Train (ID, Ponctualites)>' \
+            '<!ELEMENT Ponctualites (Entete, Ponctualite+)>' \
+            '<!ELEMENT Entete (Date, Taux, PonctuelParRetard)>' \
             '<!ELEMENT Ponctualite (Date, Taux, PonctuelParRetard)>' \
-            '<!ELEMENT ServiceNom (#PCDATA)>' \
-            '<!ELEMENT LigneID (#PCDATA)>' \
-            '<!ELEMENT LigneNom (#PCDATA)>' \
-            '<!ELEMENT TrainID (#PCDATA)>' \
+            '<!ELEMENT Nom (#PCDATA)>' \
+            '<!ELEMENT ID (#PCDATA)>' \
             '<!ELEMENT Date (#PCDATA)>' \
             '<!ELEMENT Taux (#PCDATA)>' \
             '<!ELEMENT PonctuelParRetard (#PCDATA)>' \
@@ -27,22 +28,32 @@ for line in content:
     service = [x for x in root if line["Service"] in [y.text for y in x]]
     if len(service) == 0:
         service = etree.SubElement(root, "Service")
-        service_nom = etree.SubElement(service, "ServiceNom")
+        service_nom = etree.SubElement(service, "Nom")
         service_nom.text = line["Service"]
+        service_lignes = etree.SubElement(service, "Lignes")
     else:
         service = service[0]
+        service_lignes = [x for x in service if x.tag == "Lignes"][0]
 
-    ligne = [x for x in service if line["Ligne"] in [y.text for y in x]]
+    ligne = [x for x in service_lignes if line["Ligne"] in [y.text for y in x]]
     if len(ligne) == 0:
-        ligne = etree.SubElement(service, "Ligne")
-        ligne_id = etree.SubElement(ligne, "LigneID")
+        ligne = etree.SubElement(service_lignes, "Ligne")
+        ligne_id = etree.SubElement(ligne, "ID")
         ligne_id.text = line["Ligne"]
-        ligne_nom = etree.SubElement(ligne, "LigneNom")
+        ligne_nom = etree.SubElement(ligne, "Nom")
         ligne_nom.text = line["Nom de la ligne"]
         ligne_train = etree.SubElement(ligne, "Train")
-        train_id = etree.SubElement(ligne_train, "TrainID")
+        train_id = etree.SubElement(ligne_train, "ID")
         train_id.text = line["ID"]
-        train_punctuality = etree.SubElement(ligne_train, "Ponctualite")
+        train_punctualities = etree.SubElement(ligne_train, "Ponctualites")
+        punctuality_headers = etree.SubElement(train_punctualities, "Entete")
+        header_date = etree.SubElement(punctuality_headers, "Date")
+        header_date.text = "Date"
+        header_taux = etree.SubElement(punctuality_headers, "Taux")
+        header_taux.text = "Taux de ponctualité"
+        header_atTime = etree.SubElement(punctuality_headers, "PonctuelParRetard")
+        header_atTime.text = "Nombre de voyageurs à l'heure pour un voyageur en retard"
+        train_punctuality = etree.SubElement(train_punctualities, "Ponctualite")
         punctuality_date = etree.SubElement(train_punctuality, "Date")
         punctuality_date.text = line["Date"]
         punctuality_taux = etree.SubElement(train_punctuality, "Taux")
@@ -52,7 +63,8 @@ for line in content:
     else:
         ligne = ligne[0]
         ligne_train = [x for x in ligne if line["ID"] in [y.text for y in x]][0]
-        train_punctuality = etree.SubElement(ligne_train, "Ponctualite")
+        train_punctualities = [x for x in ligne_train if x.tag == "Ponctualites"][0]
+        train_punctuality = etree.SubElement(train_punctualities, "Ponctualite")
         punctuality_date = etree.SubElement(train_punctuality, "Date")
         punctuality_date.text = line["Date"]
         punctuality_taux = etree.SubElement(train_punctuality, "Taux")
